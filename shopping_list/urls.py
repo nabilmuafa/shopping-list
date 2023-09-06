0b795395a9e94d01f85cfb49efcee71e47007530
nabilmuafa <nmuafa@gmail.com>
2023-09-06 13:40:42 +0700

    tutorial 1 selesai

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path, include
+
 """
 URL configuration for shopping_list project.
 
@@ -19,4 +21,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', include('main.urls')),
 ]

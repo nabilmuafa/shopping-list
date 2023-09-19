558346ee7d3a2606d3257127ab0f6019dc8b4987
nabilmuafa <nmuafa@gmail.com>
2023-09-19 11:41:03 +0700

    tailwind experiment 3

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -131,16 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'shopping_list/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'shopping_list/static')
 
-COMPRESS_ROOT = BASE_DIR / 'static'
+COMPRESS_ROOT = BASE_DIR / 'shopping_list/static'
 
 COMPRESS_ENABLED = True
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static')
+    os.path.join(BASE_DIR, 'shopping_list/static')
 ]
 
 STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)

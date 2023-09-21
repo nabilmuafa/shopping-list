2c29d1451c9ce0cb3e4a9e7b281e45ca754b95af
nabilmuafa <nmuafa@gmail.com>
2023-09-21 11:32:22 +0700

    try smth

--- main/views.py
+++ main/views.py
@@ -15,28 +15,28 @@ def show_main(request):
     products = Product.objects.filter(user=request.user)
     context = {
         'name': request.user.username,
-        "class": "PBP C",
-        "products": products,
+        'class': 'PBP C',
+        'products': products,
         'last_login': request.COOKIES['last_login'],
     }
 
-    return render(request, "main.html", context)
+    return render(request, 'main.html', context)
 
 def register(request):
     form = UserCreationForm()
-    if request.method == "POST":
+    if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "Your account has been successfully created!")
+            messages.success(request, 'Your account has been successfully created!')
             return redirect('main:login')
     context = {'form': form}
     return render(request, 'register.html', context)
 
 def login_user(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -58,7 +58,7 @@ def logout_user(request):
 def create_product(request):
     form = ProductForm(request.POST or None)
 
-    if form.is_valid() and request.method == "POST":
+    if form.is_valid() and request.method == 'POST':
         product = form.save(commit=False)
         product.user = request.user
         product.save()
@@ -69,16 +69,16 @@ def create_product(request):
 
 def show_xml(request):
     data = Product.objects.all()
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(serializers.serialize('xml', data), content_type='application/xml')
 
 def show_json(request):
     data = Product.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
 
 def show_xml_by_id(request, id):
     data = Product.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(serializers.serialize('xml', data), content_type='application/xml')
 
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
